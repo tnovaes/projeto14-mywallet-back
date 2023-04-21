8e39ece2e9fb6fede977c1d82db829ad4325aed6
TiagoNovaes <tiagousnovaes@gmail.com>
2023-04-21 16:40:21 -0300

    GET Balance

--- src/controllers/transactions.controllers.js
+++ src/controllers/transactions.controllers.js
@@ -8,16 +8,28 @@ export async function transaction(req, res) {
 
         const newTransaction = {
             userId: session.userId,
-            value: value*100,
+            value: value * 100,
             type,
             description
         };
 
-        await db.collection("transactions").insertOne({ newTransaction });
+        await db.collection("transactions").insertOne(newTransaction);
         res.sendStatus(200);
 
     } catch (err) {
         res.status(500).send(err.message);
     }
 };
+
+export async function balance(req, res) {
+    try {
+        const session = res.locals.session;
+
+        const userTransactions = await db.collection("transactions").find({ userId: session.userId }).sort({ _id: -1 }).toArray();
+        res.status(200).send(userTransactions);
+
+    } catch (err) {
+        res.status(500).send(err.message);
+    }
+}
 
--- src/routes/transactions.routes.js
+++ src/routes/transactions.routes.js
@@ -2,13 +2,15 @@ import { Router } from "express";
 import { authValidation } from "../middlewares/auth.middleware.js";
 import { validateSchema } from "../middlewares/validateSchema.middleware.js";
 import { transactionSchema } from "../schemas/transactions.schemas.js";
-import { transaction } from "../controllers/transactions.controllers.js";
+import { balance, transaction } from "../controllers/transactions.controllers.js";
 
 const transactionsRouter = Router();
 
 transactionsRouter.use(authValidation);
 
 transactionsRouter.post('/transactions', validateSchema(transactionSchema), transaction);
+transactionsRouter.get('/balance', balance);
+
 
 
 export default transactionsRouter;
